49958b1cbf4dff541fd81d568d456a65518c3543
luizkim <luizkim@users.noreply.github.com>
2026-09-23 22:53:50 -0300

    chore(release): align dev with rc/1.10.31-rc.11

--- src/Library/Postalcode/PostalcodeService.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace ControleOnline\Library\Postalcode;
-
-use ControleOnline\Library\Postalcode\Entity\Address;
-
-interface PostalcodeService
-{
-  public function query(string $postalCode): Address;
-}
